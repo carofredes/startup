80a859145a8c3792ea7be500efec320a377f9712
carofredes <carolinaf_13@hotmail.com>
2015-02-20 19:22:22 +0100

    Rolling Stones Albums (exercise 11)

--- 01-js-intro-and-jquery/js/jq11.js
+++ 01-js-intro-and-jquery/js/jq11.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
           $.getJSON('https://api.spotify.com/v1/search',{q:'Rolling Stones', type:'album'}, function(result) {
               console.log(result);
-             
+               $.each(result.albums.items,function(i,e){
+                  var article =$("<article/>");
+                  var aName =$("<h2>"+ e.name +"</h2>");
+                  var data = $("<div/>");
+                  
+                  var link = $("<span/>");
+                  link.html("<a href='"+ e.href+"'>Go to Spotify</a>");
+                  data.append(link.clone());
+                  link.html(e.type);
+                  data.append(link.clone());
+                  var imag = $("<img/>").attr("width", e.images[1].width).attr("src", e.images[1].url).attr("height", e.images[1].height);
+                  data.append(imag);
+                  article.append(aName,[data]);
+                  $("section").append(article.clone());
+              });
+        });
 });
