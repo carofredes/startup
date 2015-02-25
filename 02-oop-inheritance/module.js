644cd7bc61f201aa887e8fdb34aae91935d8d242
caro <carolinaf_13@hotmail.com>
2015-02-24 21:54:05 -0300

    refactoring movie class as a module

---
+++ 02-oop-inheritance/module.js
@@ -0,0 +1,87 @@
+function ObserverList() {
+    this.observerList = [];
+}
+ 
+ObserverList.prototype = {
+  add:function( obj ){
+      return this.observerList.push( obj );
+  },
+  count:function(){
+      return this.observerList.length;
+  },
+  get: function( index ){
+      if( index > -1 && index < this.observerList.length ){
+       return this.observerList[ index ];
+      }
+  },
+  indexOf : function( obj, startIndex ){
+    var i = startIndex;
+    while( i < this.observerList.length ){
+        if( this.observerList[i] === obj ){
+            return i;
+        }
+        i++;
+      }
+      return -1;
+  },
+  removeAt :function( index ){
+      this.observerList.splice( index, 1 );
+  }
+}
+
+function Observer() {
+    this.update = function(context){
+        console.log(context);
+    };
+}
+
+var movieModule = (function () {
+  // privates
+  var attributes = [];
+  var observers = new ObserverList();
+ 
+   function addObserver(observer){
+      observers.add(observer);
+  }
+ 
+  function removeObserver(observer){
+      observers.removeAt(observers.indexOf(observer, 0));
+  }
+ 
+  function notify(context){
+    var observerCount = observers.count();
+      for(var i=0; i < observerCount; i++){
+        observers.get(i).update(context);
+      }
+  }
+  
+  // Return an object exposed to the public
+  return {
+  play:function(){
+      console.log("PLAY "+attributes["title"]);
+        notify("PLAY ");
+  },
+
+  stop:function(){
+    console.log("STOP "+this.get("title"));
+       notify("STOP ");
+  },
+
+  set:function(key,value){
+    attributes[key]=value;
+  },
+
+  get:function(key){
+    return attributes[key];
+  }
+  }
+})();
+
+
+movieModule.set("title","oblivion");
+console.log(movieModule.get("title"));
+movieModule.set("director","Joseph Kosinski");
+var dir = "The director of "+movieModule.get("title")+" is "+movieModule.get("director");
+console.log(dir);
+movieModule.play();
+movieModule.stop();
