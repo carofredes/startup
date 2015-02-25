6ee9cbcb45beb2395fa790c735267998b3c5d246
caro <carolinaf_13@hotmail.com>
2015-02-25 13:53:53 -0300

    Exercise 8, create a downloadbleMovie that extends from movie

---
+++ 02-oop-inheritance/DownloadMovie.js
@@ -0,0 +1,111 @@
+function ObserverList() {
+  this.observerList = [];
+}
+ 
+ObserverList.prototype = {
+	add:function( obj ){
+  		return this.observerList.push( obj );
+	},
+ 
+	count:function(){
+  		return this.observerList.length;
+	},
+ 
+	get: function( index ){
+  		if( index > -1 && index < this.observerList.length ){
+   		 return this.observerList[ index ];
+  		}
+	},
+ 
+	indexOf : function( obj, startIndex ){
+ 		var i = startIndex;
+ 		while( i < this.observerList.length ){
+   		 	if( this.observerList[i] === obj ){
+      			return i;
+    		}
+   		 	i++;
+  		}
+	  	return -1;
+	},
+ 
+	removeAt :function( index ){
+  		this.observerList.splice( index, 1 );
+	}
+}
+
+function Observer() {
+    this.update = function(context){
+        console.log(context);
+    };
+}
+
+function Movie() {
+	this.attributes = [];
+	this.observers = new ObserverList();
+}
+
+Movie.prototype = {
+	play:function(){
+		console.log("Now playing "+this.get("title"));
+        this.notify("PLAY ");
+	},
+
+	stop:function(){
+		console.log("Now sttoped  "+this.get("title"));
+        this.notify("STOP ");
+	},
+
+	set:function(key,value){
+		this.attributes[key]=value;
+	},
+
+	get:function(key){
+		return this.attributes[key];
+	},
+	
+	addObserver :function( observer ){
+  		this.observers.add( observer );
+	},
+ 
+	removeObserver:function( observer ){
+  		this.observers.removeAt( this.observers.indexOf( observer, 0 ) );
+	},
+ 
+	notify :function( context ){
+  	var observerCount = this.observers.count();
+  		for(var i=0; i < observerCount; i++){
+    		this.observers.get(i).update( context );
+  		}
+	}
+}
+
+function DownloadableMovie(){};
+
+DownloadableMovie.prototype = new Movie();
+DownloadableMovie.prototype.constructor =  DownloadableMovie;
+DownloadableMovie.prototype.download=function(){
+        console.log("Thank You For Downloading!");
+}
+
+var myMovie = new Movie();
+var observer = new Observer();
+myMovie.addObserver(observer);
+myMovie.set("title","oblivion");
+console.log(myMovie.get("title"));
+myMovie.set("director","Joseph Kosinski");
+var dir = "The director of "+myMovie.get("title")+" is "+myMovie.get("director");
+console.log(dir);
+myMovie.play();
+myMovie.stop();
+var myMovie2 = new Movie();
+var observer = new Observer();
+myMovie2.addObserver(observer);
+myMovie2.set("title","Harry");
+myMovie2.play();
+console.log(myMovie2.get("title"));
+myMovie2.stop();
+
+var movied = new DownloadableMovie();
+movied.set("title","up");
+console.log(movied.get("title"));
+movied.download();
